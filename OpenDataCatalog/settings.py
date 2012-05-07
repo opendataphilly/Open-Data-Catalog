2ecf9ca34729b3807fdedc0f3fafe9015bc8e1ca
Jim Craner <jim@codeforamerica.org>
2012-05-07 09:38:12 -0700

    Changed comment in db settings section
    Host parameter's comment said "set to empty string for localhost" but
    this doesn't work: per the README, host needs to be set to 'localhost'
    not left null.

--- OpenDataCatalog/settings.py
+++ OpenDataCatalog/settings.py
@@ -26,7 +26,7 @@ DATABASES = {
         'NAME': 'catalog',                      # Or path to database file if using sqlite3.
         'USER': 'catalog',                      # Not used with sqlite3.
         'PASSWORD': 'passw0rd',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '',                      # Set to 'localhost' for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
